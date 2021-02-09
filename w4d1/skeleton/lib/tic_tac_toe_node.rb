e6e57ea9510a42c82b97b2fe68a0b3b84dd73852
Suiki and Timothy <teriley138@gmail.com>
2021-02-08 16:58:17 -0800

    try to initialize and create children, but fail

---
+++ w4d1/skeleton/lib/tic_tac_toe_node.rb
@@ -0,0 +1,42 @@
+require_relative 'tic_tac_toe'
+
+class TicTacToeNode
+
+
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+    @marks = [:x, :o]
+  end
+
+  def losing_node?(evaluator)
+  end
+
+  def winning_node?(evaluator)
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def other_mark
+    return :o if @next_mover_mark == :x
+    return :x if @next_mover_mark == :o
+  end
+
+
+  def children
+    kids = []
+    marks_dup = @marks.clone
+    (0...3).each do |row|
+      (0...3).each do |col|
+        if @board.empty?([row, col])
+
+          node = TicTacToeNode.new(@board, other_mark, [row, col])
+          kids << node
+        end
+      
+      end
+    end
+    kids
+  end
+end
