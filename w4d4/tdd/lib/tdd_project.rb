e47d0cd3b68baffbee4028599390cd559a06ca9f
natkozak <natkozak0@gmail.com>
2021-02-11 16:00:37 -0800

    Finish TowerOfHanoi#won? specs

--- w4d4/tdd/lib/tdd_project.rb
+++ w4d4/tdd/lib/tdd_project.rb
@@ -34,3 +34,48 @@ def transpose(array)
   end
   return transposed
 end
+
+def stock_picker(days)
+  price = []
+  max = 0
+  (0...days.length).each do |i|
+    (i+1...days.length).each do |j|
+      if days[j] - days[i] > 0 && days[j] - days[i] > max
+        price = [i, j]
+        max = days[j] - days[i]
+      end
+    end
+  end
+  price
+end
+
+class TowersOfHanoi
+  attr_accessor :rods
+
+  def initialize
+    @rods = Array.new(3) {Array.new(3)}
+    @rods[0] = [3,2,1]
+  end
+
+  def prompt
+    print "Please enter a starting rod and an ending rod"
+    input = gets.chomp.split.map(:to_i)
+  end
+
+  def move
+    unless won?
+      begin
+        input = prompt
+        input.each do |i|
+          @rods[i].pop 
+        end
+      rescue InputError
+        retry
+      end
+    end
+  end
+
+  def won?
+    return @rods[2] == [3,2,1]
+  end
+end
--- w4d4/tdd/spec/tdd_project_spec.rb
+++ w4d4/tdd/spec/tdd_project_spec.rb
@@ -37,4 +37,71 @@ describe "my_transpose" do
     ])
 
   end
+
+end
+
+describe "stock_picker" do
+  it "should return most profitable pair" do
+    expect(stock_picker([1,2,3,4,5])).to eq([0, 4])
+    expect(stock_picker([3,2,99,500,5,1000,1])).to eq([1, 5])
+    expect(stock_picker( [100, 40, 300, 500, 1] ) ).to eq([1, 3])
+  end
+end
+
+describe TowersOfHanoi do 
+  subject(:tower) {TowersOfHanoi.new}
+
+  describe "#initialize" do 
+    it "should initialize the board to have 3 rods and 3 discs" do
+      expect(tower.rods.length).to eq(3)
+      expect(tower.rods[0].length).to eq(3)
+    end
+
+    it "should initialize the discs inside the first rod" do
+      expect(tower.rods[0]).to eq([3,2,1])
+    end
+  end
+  
+  # describe "#prompt" do
+
+  #   it "should print \"Please enter a starting rod and an ending rod\"" do
+  #     input = double("0 2\n", :chomp=>"0 2")
+  #     allow(tower).to receive(:gets).and_return(input)
+
+  #     expect { tower.prompt }.to output("Please enter a starting rod and an ending rod").to_stdout
+  #   end
+
+  #   it "it should call gets.chomp to get input from the user" do
+  #     input = double("0 2\n", :chomp=>"0 2")
+  #     allow(tower).to receive(:gets).and_return(input)
+
+  #     expect(input).to receive(:chomp)
+  #     expect(tower).to receive(:gets)
+  #   end
+  # end
+
+  describe "#won?" do
+
+    context "when the game is won" do
+      
+      it "should return true" do
+        tower.rods[2] = [3,2,1]
+        expect(tower.rods[2]).to eq([3,2,1])
+        expect(tower.won?).to eq(true)
+      end
+    end
+
+    context "when the game is not won" do
+      
+      it "should return false" do
+        tower.rods[2] = []
+        expect(tower.rods[2]).to_not eq([3,2,1])
+        expect(tower.won?).to eq false
+      end
+    end
+  end
+
+  describe "#move" do
+    it "should contain the main game loop"
+  end
 end
