8466ce5723e14020649bf42c58df462e02fb9d99
Mike <michaeldeanmdmph@gmail.com>
2021-02-17 17:30:54 -0800

    add next commit

--- w5d3/AA_questions/questions.rb
+++ w5d3/AA_questions/questions.rb
@@ -157,6 +157,50 @@ class Reply
     data = QuestionsDatabase.instance.execute("SELECT * FROM replies")
     data.map { |datum| Reply.new(datum) } #makes 2D array out of data
   end
+  
+    def self.find_by_id(id)
+      reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT
+          *
+        FROM
+          replies
+        WHERE
+          id = ?
+      SQL
+      return nil unless reply.length > 0
+  
+      Reply.new(reply.first) # play is stored in an array!
+    end
+  
+    def self.find_by_author_id(author_id)
+      reply = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+        SELECT
+          *
+        FROM
+          replies
+        WHERE
+          author_id = ?
+      SQL
+      return nil unless reply.length > 0
+  
+      # Reply.new(reply.first) # play is stored in an array!
+      reply.map { |rep| Reply.new(rep) } # maping into an array
+    end
+  
+    def self.find_by_question_id(question_id)
+      reply = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+        SELECT
+          *
+        FROM
+          replies
+        WHERE
+          question_id = ?
+      SQL
+      return nil unless reply.length > 0
+  
+      # Reply.new(reply.first) # play is stored in an array!
+      reply.map { |rep| Reply.new(rep) } # maping into an array
+    end
 
   def initialize(options)
     @id = options['id']
@@ -166,48 +210,32 @@ class Reply
     @parent_reply_id = options['parent_reply_id']
   end
 
-  def self.find_by_id(id)
+  def author(author_id)
+    User.find_by_id(author_id)
+  end
+
+  def question(question_id)
+    Question.find_by_id(question_id)
+  end
+
+  def parent_reply(parent_reply_id)
+    Reply.find_by_id(parent_reply_id)
+  end
+
+  def child_replies(id)
     reply = QuestionsDatabase.instance.execute(<<-SQL, id)
-      SELECT
-        *
-      FROM
-        replies
-      WHERE
-        id = ?
-    SQL
-    return nil unless reply.length > 0
+    SELECT
+      *
+    FROM
+      replies
+    WHERE
+      id = ?
+  SQL
+  return nil unless reply.length > 0
 
-    Reply.new(reply.first) # play is stored in an array!
-  end
-
-  def self.find_by_author_id(author_id)
-    reply = QuestionsDatabase.instance.execute(<<-SQL, author_id)
-      SELECT
-        *
-      FROM
-        replies
-      WHERE
-        author_id = ?
-    SQL
-    return nil unless reply.length > 0
-
-    # Reply.new(reply.first) # play is stored in an array!
-    reply.map { |rep| Reply.new(rep) } # maping into an array
-  end
-
-  def self.find_by_question_id(question_id)
-    reply = QuestionsDatabase.instance.execute(<<-SQL, question_id)
-      SELECT
-        *
-      FROM
-        replies
-      WHERE
-        question_id = ?
-    SQL
-    return nil unless reply.length > 0
-
-    # Reply.new(reply.first) # play is stored in an array!
-    reply.map { |rep| Reply.new(rep) } # maping into an array
+  # Reply.new(reply.first) # play is stored in an array!
+  reply.map { |rep| Reply.new(rep) } # maping into an array
+    
   end
 
 end
