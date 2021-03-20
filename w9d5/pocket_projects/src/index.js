b5100a54efcd1d6e8473b7234e9efcb592c89d39
natkozak <natkozak0@gmail.com>
2021-03-19 17:00:07 -0700

    Make todos persist to localstorage and stay on page

--- w9d5/pocket_projects/src/index.js
+++ w9d5/pocket_projects/src/index.js
@@ -1,3 +1,4 @@
 import warmUp from "./warmup";
 import Clock from "./clock";
 import DropDown from "./drop_down";
+import TodoList from "./todo_list";
--- w9d5/pocket_projects/src/todo_list.js
+++ w9d5/pocket_projects/src/todo_list.js
@@ -1,2 +1,25 @@
+const lsTodos = JSON.parse(localStorage.getItem('todos')) || [];
+const todoClass = document.querySelector('.todos');
 
 
+function addTodo (e) {
+  e.preventDefault();
+  const input = document.querySelector('input[name="add-todo"]');
+  const value = input.value;
+  const todos = { value, done: false };
+  lsTodos.push(todos);
+  localStorage.setItem("todos", JSON.stringify(lsTodos));
+
+  populateList();
+}
+
+function populateList () {
+  todoClass.innerHTML = lsTodos.map((item) => {
+    return `<li>${item.value}</li>`;
+  }).join(" ");
+}
+
+
+const submitButton = document.querySelector('.add-todo-form');
+submitButton.addEventListener("submit", addTodo);
+populateList();
