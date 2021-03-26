ef57bf47d6460acaabf16244bafd6cd178825617
natkozak <natkozak0@gmail.com>
2021-03-26 14:22:42 -0700

    finish Todo Form

--- w10d4/todo_redux/frontend/components/todos/todo_list.jsx
+++ w10d4/todo_redux/frontend/components/todos/todo_list.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import TodoListItem from "./../todo_list/todo_list_item";
+import TodoForm from "./../todo_list/todo_form";
 
 class TodoList extends React.Component {
   render() {
+    console.log(this.props);
+    const items = this.props.todos.map(todo => {
+      return <TodoListItem todo={todo} key={todo.id}/>
+    })
     return (
       <div>I am a Todo List
         <ul>
-          {this.props.todos.map(todo => {
-            <li key={todo.id}>
-              Title: {todo.title}
-            </li>
-          })}
+          {/* {this.props.todos.map(todo => <li key={todo.id}>Title: {todo.title}</li>)} */}
+          {items}
+        </ul>
+        <h1>Todo Form</h1>
+        <ul>
+          <TodoForm receiveTodo={this.props.receiveTodo}/>
         </ul>
       </div>
     )
