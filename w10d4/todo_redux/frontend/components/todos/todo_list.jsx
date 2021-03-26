0cf9c8fbd21dbfe89b59674fc823d4df0148ca23
natkozak <natkozak0@gmail.com>
2021-03-26 12:22:48 -0700

    Render Todo List

--- w10d4/todo_redux/frontend/components/todos/todo_list.jsx
+++ w10d4/todo_redux/frontend/components/todos/todo_list.jsx
@@ -4,10 +4,30 @@ class TodoList extends React.Component {
   render() {
     return (
       <div>I am a Todo List
-
+        <ul>
+          {this.props.todos.map(todo => {
+            <li key={todo.id}>
+              Title: {todo.title}
+            </li>
+          })}
+        </ul>
       </div>
     )
   }
 }
 
 export default TodoList;
+
+// import React from 'react';
+
+// const TodoList = (props) => {
+//   return (
+//     <div> <h1>I am a Todo List</h1>
+//       <ul>
+//         {props.todos.map(todo => <li>Title: {todo.title}</li>)}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default TodoList;
